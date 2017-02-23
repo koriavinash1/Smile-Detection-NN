eb864d894c0c394f2f7af82ed9169507ec7237de
koriavinash <koriavinash1@gmail.com>
2017-02-23 18:49:03 +0530

    CNN

--- test/TF_test.py
+++ test/TF_test.py
@@ -10,6 +10,7 @@ cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 timageh,timagew = 100,100#test image height and width(h,w)....
 path = './yalefaces'
+cap=cv2.VideoCapture(0)
 images={'image':[],'label':[]}
 
 def get_image_data():
@@ -62,6 +63,15 @@ def fully_connected_net_no_hidden_layers():
 		accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 		print "accuracy................", accuracy.eval(feed_dict={x: sess.run(images['image']), y_: images['label']})
 
+	ret, testimg = cap.read()
+	cv2.waitKey(50)
+	testimg = cv2.cvtColor(testimg, cv2.COLOR_BGR2GRAY)
+	faces = faceCascade.detectMultiScale(testimg)
+	for (x, y, w, h) in faces:
+		timage = tf.reshape(np.array(cv2.resize(testimg[y:y+h, x:x+w],(timagew, timageh), interpolation = cv2.INTER_CUBIC), dtype='float32'), [-1])
+	y = tf.matmul([timage], W) + b
+	print "final_result....................................",sess.run(y)
+
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(initial)
@@ -77,15 +87,42 @@ def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 def Convolutional_net():
+	x = tf.placeholder(tf.float32, shape=[None, timageh*timagew])
+	y_ = tf.placeholder(tf.float32, shape=[None, 7])
 	W_conv1 = weight_variable([5, 5, 1, 32])
 	b_conv1 = bias_variable([32])
-	x_image = tf.reshape(sess.run(images['image'][0]), [-1,timageh,timagew,1])
-	h_conv1 = tf.nn.relu(conv2d(images['image'], W_conv1) + b_conv1)
+	x_image = tf.reshape(x, [-1,timageh,timagew,1])
+	h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 	h_pool1 = max_pool_2x2(h_conv1)
-	cv2.imshow("temp", sess.run(h_pool1))
-	cv2.waitKey(0)
-	pass
+
+	W_conv2 = weight_variable([5, 5, 32, 64]) #second layer
+	b_conv2 = bias_variable([64])
+
+	h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+	h_pool2 = max_pool_2x2(h_conv2)
+
+	W_fc1 = weight_variable([25 * 25 * 64, 1024])
+	b_fc1 = bias_variable([1024])
+
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 25*25*64])
+	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+	keep_prob = tf.placeholder(tf.float32)
+	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+	W_fc2 = weight_variable([1024, 7])
+	b_fc2 = bias_variable([7])
+
+	y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+	sess.run(tf.global_variables_initializer())
+	for _ in range(1000):
+		train_step.run(feed_dict={x: np.array(sess.run(images['image']),dtype='float32'), y_: images['label'],keep_prob: 0.5})
+		correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+		accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		train_accuracy = accuracy.eval(feed_dict={x:sess.run(images['image']), y_: images['label'], keep_prob: 0.5})
+		print "step %d, training accuracy %g"%(_, train_accuracy)
 
 get_image_data()
 fully_connected_net_no_hidden_layers()
-# Convolutional_net()
+Convolutional_net()
